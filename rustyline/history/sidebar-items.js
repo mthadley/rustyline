327df5ad5c6e70e113105b20108b839f8e840824
Katsu Kawakami <kkawa1570@gmail.com>
2016-10-24 02:47:31 +0000

    rebuild pages at f78f48d

--- rustyline/history/sidebar-items.js
+++ rustyline/history/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["Direction",""]],"struct":[["History",""]]});
+initSidebarItems({"enum":[["Direction",""]],"struct":[["History","Current state of the history."],["Iter","History iterator."]]});
