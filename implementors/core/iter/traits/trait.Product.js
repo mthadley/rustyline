8cbc723f9232176c503baca22cd4902f63eb5c42
Katsu Kawakami <kkawa1570@gmail.com>
2016-11-20 14:51:50 +0000

    rebuild pages at 1ae47dc

--- implementors/core/iter/traits/trait.Product.js
+++ implementors/core/iter/traits/trait.Product.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["void"] = [];implementors["libc"] = [];implementors["nix"] = [];
+implementors["void"] = [];implementors["libc"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
